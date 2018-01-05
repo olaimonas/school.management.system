58dfa0a8c498b0fda90912e71e886ee5ea766fde
olaimonas <olaimonas@gmail.com>
2018-01-05 12:13:36 +0200

    Update

--- src/main/java/school/management/system/util/impl/PropertiesConfig.java
+++ src/main/java/school/management/system/util/impl/PropertiesConfig.java
@@ -7,23 +7,34 @@ import java.util.Properties;
 
 public class PropertiesConfig {
 	
-    public String getExceptionMessage(String message) {
-
-    	String rootPath = Thread.currentThread().getContextClassLoader().getResource("Application.properties").getPath();
-    	String appConfigPath = rootPath + "Application.properties";
-    	
-        Properties appProps = new Properties();
-        try {
-			appProps.load(new FileInputStream(appConfigPath));
-			return appProps.getProperty(message);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        return appProps.getProperty(message);
+	private static final String PROPERTIES_FILENAME = "Application.properties";
+	
+    private static ClassLoader objClassLoader = null;
+    private static Properties properties = new Properties();
+    
+    public PropertiesConfig() {
+    	objClassLoader = getClass().getClassLoader();
+    }
+    
+    public static String readKey(String propertiesFilename, String key) {
+    	if(propertiesFilename != null && !propertiesFilename.trim().isEmpty() && key != null && !key.trim().isEmpty()) {
+    		// Try-with-resource
+    		try(FileInputStream objFileInputStream = new FileInputStream(objClassLoader.getResource(propertiesFilename).getFile());) {
+    			// Load file into properties
+    			properties.load(objFileInputStream);
+    			// Get the value of key
+    			return String.valueOf(properties.get(key));
+    		} catch(FileNotFoundException e) {
+    			e.printStackTrace();
+    		} catch(IOException e) {
+    			e.printStackTrace();
+    		}
+    	}
+    	return null;
     }
 
+	public static String getPropertiesFilename() {
+		return PROPERTIES_FILENAME;
+	}
+	
 }
