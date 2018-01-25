99a587fba3d935b7ab9558654c0a8b933ebcac05
Laimonas <olaimonas@gmail.com>
2018-01-25 17:08:44 +0200

    Corrected "NoSuchTeacherException" usage in its implementation.

--- src/main/java/school/management/system/exception/NoSuchTeacherException.java
+++ src/main/java/school/management/system/exception/NoSuchTeacherException.java
@@ -6,7 +6,7 @@ public class NoSuchTeacherException extends RuntimeException {
 
     private static final long serialVersionUID = -6143986615125941932L;
 
-    private static String message = PropertiesConfig.readKey(PropertiesConfig.PROPERTIES_FILENAME, "noSuchTeacherExceptiosn");
+    private static String message = PropertiesConfig.readKey(PropertiesConfig.PROPERTIES_FILENAME, "noSuchTeacherException");
 
     public NoSuchTeacherException() {
         this(message);
